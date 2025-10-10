c72278236dd23b4c343a980f760ebbbe83370805
Divyanshu951 <divyanshuk9515@gmail.com>
2025-10-10 20:30:29 +0530

    updated files

---
+++ 04-steps/starter/steps/src/App.js
@@ -0,0 +1,48 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  let step = 1;
+
+  function handlePrevious() {
+    step--;
+    console.log(step);
+  }
+
+  function handlenext() {
+    step++;
+    console.log(step);
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlenext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
