76d310f253dd97c368b2dba3a40ae3cdcdace70f
Divyanshu951 <divyanshuk9515@gmail.com>
2025-10-15 18:30:49 +0530

    updated files

--- _SideProjects/todo-list/src/App.js
+++ _SideProjects/todo-list/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input />
     </div>
   );
 }
 
-export default App;
+function Input() {
+  const [input, setInput] = useState("");
+
+  return (
+    <form>
+      <input
+        placeholder="Add a task ..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button className="submit">+</button>
+    </form>
+  );
+}
