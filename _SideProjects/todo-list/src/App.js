d11c3a2f7d43fc1267bc86e7b547819673312d30
Divyanshu951 <divyanshuk9515@gmail.com>
2025-10-16 18:05:23 +0530

    updated files

--- _SideProjects/todo-list/src/App.js
+++ _SideProjects/todo-list/src/App.js
@@ -1,24 +1,51 @@
 import { useState } from "react";
 
+const data = [
+  { id: 1, description: "DSA", status: false },
+  { id: 2, description: "React", status: true },
+];
+
 export default function App() {
   return (
     <div>
-      <Input />
+      <Form />
+      <ItemList />
     </div>
   );
 }
 
-function Input() {
-  const [input, setInput] = useState("");
+function Form() {
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    setDescription("");
+
+    // const newitem =
+  }
 
   return (
     <form>
       <input
         placeholder="Add a task ..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
-      <button className="submit">+</button>
+      <button onClick={handleSubmit} className="submit">
+        +
+      </button>
     </form>
   );
+}
+
+function Item() {
+  const prop = { id: 1, description: "DSA", status: false };
+
+  return;
+}
+
+function ItemList() {
+  return <ul>{/* <li>{data.map(item => )}</li> */}</ul>;
 }
