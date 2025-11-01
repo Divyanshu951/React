88c808427d6fd0e32e8e2761512dcf57e01842ef
Divyanshu951 <divyanshuk9515@gmail.com>
2025-11-01 12:51:39 +0530

    updated files

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -8,37 +8,46 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((prevStep) => prevStep + 1);
   }
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((prevStep) => prevStep - 1);
   }
 
   const buttonStyle = { background: "#7950F2", color: "#fff" };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        {isOpen ? "\u00D7" : "\u2600"}
+      </button>
 
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button onClick={handlePrevious} style={buttonStyle}>
-          Previous
-        </button>
-        <button onClick={handleNext} style={buttonStyle}>
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button onClick={handlePrevious} style={buttonStyle}>
+              Previous
+            </button>
+            <button onClick={handleNext} style={buttonStyle}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
