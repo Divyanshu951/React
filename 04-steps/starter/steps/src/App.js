794d34c3480c123684c3ccdae2e04584354fb2e5
Divyanshu951 <divyanshuk9515@gmail.com>
2025-11-02 20:29:26 +0530

    updated files

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -7,47 +7,77 @@ const messages = [
 ];
 
 export default function App() {
+  // const [step, setStep] = useState(1);
+  // const [isOpen, setIsOpen] = useState(true);
+
+  // function handleNext() {
+  //   if (step < 3) setStep((prevStep) => prevStep + 1);
+  // }
+
+  // function handlePrevious() {
+  //   if (step > 1) setStep((prevStep) => prevStep - 1);
+  // }
+
+  // const buttonStyle = { background: "#7950F2", color: "#fff" };
+
+  // Variables
   const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
+  const [count, setCount] = useState(0);
 
-  function handleNext() {
-    if (step < 3) setStep((prevStep) => prevStep + 1);
-  }
-
-  function handlePrevious() {
-    if (step > 1) setStep((prevStep) => prevStep - 1);
-  }
-
-  const buttonStyle = { background: "#7950F2", color: "#fff" };
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen((is) => !is)}>
-        {isOpen ? "\u00D7" : "\u2600"}
-      </button>
-
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
-
-          <p className="message">
-            Step {step} : {messages[step - 1]}
-          </p>
-
-          <div className="buttons">
-            <button onClick={handlePrevious} style={buttonStyle}>
-              Previous
-            </button>
-            <button onClick={handleNext} style={buttonStyle}>
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+      {/* <div>
+        <button onClick={() => setStep((prevStep) => prevStep - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((prevStep) => prevStep + 1)}>+</button>
+      </div> */}
+      <input
+        type="range"
+        min="1"
+        max="10"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <span>{step}</span>
+      <div>
+        <button onClick={() => setCount((prevCount) => prevCount - step)}>
+          -
+        </button>
+        <span>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+        </span>
+        <button onClick={() => setCount((prevCount) => prevCount + step)}>
+          +
+        </button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 ||
+        (step !== 1 && (
+          <button
+            onClick={() => {
+              setCount(0);
+              setStep(1);
+            }}
+          >
+            Reset
+          </button>
+        ))}
     </>
   );
 }
--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,34 +1,93 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "Charger", quantity: 2, packed: false },
-// ];
-
-const pizzas = ["ljdhfgklhgkjh", "Margherita", "Pepperoni", "Veggie"];
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 2, packed: false },
+];
 
 export default function App() {
   return (
-    <ul className="app">
-      {pizzas.map((item) => (
-        <Start text={item} key={item} />
-      ))}
-    </ul>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   );
 }
 
-function Start({ text }) {
+function Logo() {
+  return <h1>🚢 Far Away 🌴</h1>;
+}
+
+function Form() {
+  // S1
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+
+    console.log(newItem);
+  }
+
   return (
-    <li
-      style={{
-        color: "red",
-        fontSize: "40px",
-        textTransform: "uppercase",
-        borderBottom: "2px solid green",
-      }}
-    >
-      {text}
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((val) => (
+          <option key={val} value={val}>
+            {val}
+          </option>
+        ))}
+      </select>
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        type="text"
+        placeholder="Item..."
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
     </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      You have X items in your list, and you have already packed X (X%)
+    </footer>
   );
 }
