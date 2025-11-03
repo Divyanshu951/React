d9c6855ecb01a86452d2ba9f4825722709ffa037
Divyanshu951 <divyanshuk9515@gmail.com>
2025-11-04 00:43:08 +0530

    final code

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -4,12 +4,6 @@ import Stats from "./Stats";
 import PackingList from "./PackingList";
 import Form from "./Form";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "Charger", quantity: 2, packed: false },
-// ];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
